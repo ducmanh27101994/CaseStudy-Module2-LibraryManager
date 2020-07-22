3f9a07beda5ee07ae024e07c2db44a1bdf933fc1
ducmanh27101994 <ducmanh27101994@gmail.com>
2020-07-22 18:36:31 +0700

    tao class borrow

--- index.php
+++ index.php
@@ -5,6 +5,18 @@ use Lib\Controller\StudentController;
 require __DIR__ . "/vendor/autoload.php";
 $studentController = new StudentController();
 $page = isset($_REQUEST['page']) ? $_REQUEST['page'] : "";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<?php
 switch ($page) {
     case 'list-student':
         $studentController->viewStudent();
@@ -21,3 +33,7 @@ switch ($page) {
     default:
         $studentController->viewStudent();
 }
+?>
+</body>
+</html>
+
---
+++ src/Model/Borrow.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Lib\Model;
+
+
+class Borrow
+{
+
+}
