203ba85bebfbf5b16c678a5fd301bada0386be68
ducmanh27101994 <ducmanh27101994@gmail.com>
2020-07-23 22:28:03 +0700

    fix code

--- src/View/Menu/menu.php
+++ src/View/Menu/menu.php
@@ -1,7 +1,9 @@
-<a href="index.php?page=list-book">List Book</a>
 <a href="index.php?page=list-category">List Category</a>
+<a href="index.php?page=list-book">List Book</a>
 <a href="index.php?page=list-student">List Student</a>
-<a href="index.php?page=list-borrow">List Borrow</a>
+<a href="index.php?page=add-borrow">Add Borrow</a>
+<a href="index.php?page=list-borrow">Return Borrow</a>
+
 <a href="index.php?page=list-detail">List Detail</a>
 <a href="index.php?page=fullList-detail">Full List Borrow</a>
 <a href="index.php?page=borrowBook-detail">Borrow Books</a>
