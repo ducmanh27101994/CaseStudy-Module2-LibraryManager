186fa2e7879db05bd897794c9940bec0308d6f4f
ducmanh27101994 <ducmanh27101994@gmail.com>
2020-07-23 23:19:05 +0700

    fix menu

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -21,4 +21,14 @@ class UserManager
         $statement->execute();
         return $statement->fetch();
     }
+
+    function addUser($fullname,$username,$password,$phone){
+        $sql = "INSERT INTO `tbl_users`(`fullname`, `username`, `password`, `phone`) VALUES (:fullname,:username,:password,:phone)";
+        $statement = $this->databaseUser->connect()->prepare($sql);
+        $statement->bindParam(':fullname',$fullname);
+        $statement->bindParam(':username',$username);
+        $statement->bindParam(':password',$password);
+        $statement->bindParam(':phone',$phone);
+        $statement->execute();
+    }
 }
--- src/View/Menu/menu.php
+++ src/View/Menu/menu.php
@@ -1,8 +1,12 @@
-<a href="index.php?page=list-book">List Book</a>
 <a href="index.php?page=list-category">List Category</a>
+<a href="index.php?page=list-book">List Book</a>
 <a href="index.php?page=list-student">List Student</a>
-<a href="index.php?page=list-borrow">List Borrow</a>
-<a href="index.php?page=list-detail">List Detail</a>
+
+
+<a href="index.php?page=add-borrow">Add Borrow</a>
+<a href="index.php?page=list-borrow">Return Borrow</a>
+
+
 <a href="index.php?page=fullList-detail">Full List Borrow</a>
 <a href="index.php?page=borrowBook-detail">Borrow Books</a>
 <a href="index.php?page=giveBookBack-detail">Give Book Back</a>
