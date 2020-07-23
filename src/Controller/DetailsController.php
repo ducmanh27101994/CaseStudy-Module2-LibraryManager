5e4e766776d87c608c2e058d101c9939f2502966
ducmanh27101994 <ducmanh27101994@gmail.com>
2020-07-23 16:04:42 +0700

    addde

--- src/Controller/DetailsController.php
+++ src/Controller/DetailsController.php
@@ -4,22 +4,35 @@
 namespace Lib\Controller;
 
 
+use Lib\Model\BookManager;
+use Lib\Model\BorrowManager;
 use Lib\Model\DetailManager;
 
 class DetailsController
 {
     protected $detailManager;
-
+    protected $bookManager;
+    protected $borrowManager;
 
     public function __construct()
     {
         $this->detailManager = new DetailManager();
+        $this->bookManager = new BookManager();
+        $this->borrowManager = new BorrowManager();
     }
 
-//    function addDetail(){
-//        if ($_SERVER['REQUEST_METHOD']=='GET'){
-//
-//        }
-//    }
-//
+    function addDetail()
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+           $books = $this->bookManager->getAll();
+           $borrows = $this->borrowManager->getAllBorrow();
+            include_once "src/View/tbl_details/addDetails.php";
+        } else {
+            $book_id = $_REQUEST['book_id'];
+            $borrow_id = $_REQUEST['borrow_id'];
+            $this->detailManager->addDetail($book_id,$borrow_id);
+            header('location:index.php?page=list-borrow');
+        }
+    }
+
 }
