53a81c0e8f0e72bfc2baaac417670e4f4941b30c
cuong6218 <cuong6218@domain.com>
2020-07-23 07:57:42 +0700

    them giao dien list-category, them function hien thi category trong class CategoryController

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Lib\Controller;
+
+use Lib\Model\CategoryManager;
+
+class CategoryController
+{
+    private $categoryManager;
+    function __construct()
+    {
+        $this->categoryManager = new CategoryManager();
+    }
+    function viewCategory()
+    {
+        $listCategory = $this->categoryManager->getAll();
+        include_once('src/View/tbl_category/list-category.php');
+    }
+}
