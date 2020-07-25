507fe438c201f575ff2ab5939c086ea0832ba7f4
ducmanh27101994 <ducmanh27101994@gmail.com>
2020-07-25 10:50:03 +0700

    cate6

--- src/View/tbl_students/add-student.php
+++ src/View/tbl_students/add-student.php
@@ -1,20 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+<!--<form method="post">-->
+<!--    <input type="text" name="name" placeholder="student name" required/>-->
+<!--    <input type="text" name="class" placeholder="student class" required/>-->
+<!--    <input type="text" name="phone" placeholder="student phone" required/>-->
+<!--    <input type="text" name="address" placeholder="student address" required/>-->
+<!--    <button type="submit">Add Student</button>-->
+<!--</form>-->
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 
-<body>
-    <form method="post">
-        <input type="text" name="name" placeholder="student name" required/>
-        <input type="text" name="class" placeholder="student class" required/>
-        <input type="text" name="phone" placeholder="student phone" required/>
-        <input type="text" name="address" placeholder="student address" required/>
-        <button type="submit">Add Student</button>
+<br><br><br>
+<div id="container-book" class="container">
+    <h3 id="book-h3">Add Student</h3>
+    <form method="post" class="was-validated">
+        <div class="form-group">
+            <label for="uname">Student Name:</label>
+            <input type="text" class="form-control" id="uname" placeholder="Enter student name" name="name" required>
+            <div class="valid-feedback">Valid.</div>
+            <div class="invalid-feedback">Please fill out this field.</div>
+        </div>
+        <div class="form-group">
+            <label for="uname">Class:</label>
+            <input type="text" class="form-control" id="uname" placeholder="Enter class" name="class" required>
+            <div class="valid-feedback">Valid.</div>
+            <div class="invalid-feedback">Please fill out this field.</div>
+        </div>
+        <div class="form-group">
+            <label for="uname">Phone:</label>
+            <input type="text" class="form-control" id="uname" placeholder="Enter phone" name="phone" required>
+            <div class="valid-feedback">Valid.</div>
+            <div class="invalid-feedback">Please fill out this field.</div>
+        </div>
+        <div class="form-group">
+            <label for="uname">Address:</label>
+            <input type="text" class="form-control" id="uname" placeholder="Enter address" name="address" required>
+            <div class="valid-feedback">Valid.</div>
+            <div class="invalid-feedback">Please fill out this field.</div>
+        </div>
+
+
+        <button id="add-all" type="submit" class="btn btn-primary">Add Student</button>
     </form>
-</body>
-
-</html>
+</div>
